7c5d3a1c2b053343eb5e55b36304d77a1848b04a
@Aman <59781323+aman-sandhu@users.noreply.github.com>
2025-11-18 22:36:59 +0000

    Update __init__.py

--- custom_components/visonicgui/__init__.py
+++ custom_components/visonicgui/__init__.py
@@ -1,40 +1,64 @@
 import logging
-import threading
-from datetime import timedelta
-from datetime import datetime
-
-from homeassistant.core import HomeAssistant
+import voluptuous as vol
 from homeassistant.helpers import discovery
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
-from homeassistant.config_entries import ConfigEntry
-
-import voluptuous as vol
-
-from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+REQUIREMENTS = ['visonicalarm2==3.3.1', 'python-dateutil==2.7.3']
+
+# Configuration Constants
+CONF_HOST = 'host'
+CONF_NAME = 'name'
+CONF_APP_ID = 'app_id'
+CONF_USER_CODE = 'user_code'
+CONF_USER_EMAIL = 'user_email'
+CONF_USER_PASSWORD = 'user_password'
+CONF_PANEL_ID = 'panel_id'
+CONF_PARTITION = 'partition'
+CONF_EVENT_HOUR_OFFSET = 'event_hour_offset'
+
+DEFAULT_NAME = 'Visonic Alarm'
+DEFAULT_PARTITION = 'ALL'
+
+# Define DOMAIN
+DOMAIN = 'visonicgui'
+
 HUB = None
 
-# Configuration schema for the config flow
+# Config schema
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Required('host'): cv.string,
-        vol.Required('app_id'): cv.string,
-        vol.Required('user_code'): cv.string,
-        vol.Required('user_email'): cv.string,
-        vol.Required('user_password'): cv.string,
-        vol.Required('panel_id'): cv.string,
-        vol.Optional('partition', default='ALL'): cv.string,
-        vol.Optional('name', default='Visonic Alarm'): cv.string,
-        vol.Optional('no_pin_required', default=False): cv.boolean,
-        vol.Optional('event_hour_offset', default=0): vol.All(vol.Coerce(int), vol.Range(min=-24, max=24)),
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_APP_ID): cv.string,
+        vol.Required(CONF_USER_CODE): cv.string,
+        vol.Required(CONF_USER_EMAIL): cv.string,
+        vol.Required(CONF_USER_PASSWORD): cv.string,
+        vol.Required(CONF_PANEL_ID): cv.string,
+        vol.Optional(CONF_PARTITION, default=DEFAULT_PARTITION): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_EVENT_HOUR_OFFSET, default=0): vol.All(vol.Coerce(int), vol.Range(min=-24, max=24)),
     }),
 }, extra=vol.ALLOW_EXTRA)
 
-# The Visonic Alarm Hub class
+def setup(hass, config):
+    """ Setup the Visonic Alarm component."""
+    from visonic import alarm as visonicalarm  # Import here to avoid circular imports
+    global HUB
+    HUB = VisonicAlarmHub(config[DOMAIN], visonicalarm)
+    
+    if not HUB.connect():
+        return False
+
+    HUB.update()
+
+    # Load the supported platforms (alarm_control_panel, sensor, etc.)
+    for component in ('sensor', 'alarm_control_panel'):
+        discovery.load_platform(hass, component, DOMAIN, {}, config)
+
+    return True
+
 class VisonicAlarmHub(Entity):
     """ A Visonic Alarm hub wrapper class. """
 
@@ -44,14 +68,13 @@ class VisonicAlarmHub(Entity):
         self._visonicalarm = visonicalarm
         self._last_update = None
         self._lock = threading.Lock()
-
-        self.alarm = visonicalarm.System(domain_config['host'],
-                                         domain_config['app_id'],
-                                         domain_config['user_code'],
-                                         domain_config['user_email'],
-                                         domain_config['user_password'],
-                                         domain_config['panel_id'],
-                                         domain_config['partition'])
+        self.alarm = visonicalarm.System(domain_config[CONF_HOST],
+                                         domain_config[CONF_APP_ID],
+                                         domain_config[CONF_USER_CODE],
+                                         domain_config[CONF_USER_EMAIL],
+                                         domain_config[CONF_USER_PASSWORD],
+                                         domain_config[CONF_PANEL_ID],
+                                         domain_config[CONF_PARTITION])
 
     def connect(self):
         """ Setup a connection to the Visonic API server. """
@@ -73,8 +96,10 @@ class VisonicAlarmHub(Entity):
         try:
             if self.alarm.is_token_valid == False:
                 self.alarm.connect()
+            
             self.alarm.update_status()
             self.alarm.update_devices()
+
             self._last_update = datetime.now()
         except Exception as ex:
             _LOGGER.error('Update failed: %s', ex)
@@ -82,27 +107,5 @@ class VisonicAlarmHub(Entity):
 
     @property
     def name(self):
-        """ Return the name of the hub. """
+        """ Return the name of the hub."""
         return "Visonic Alarm Hub"
-
-
-# This method handles the setup of the integration when Home Assistant loads it
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Visonic Alarm integration."""
-    _LOGGER.debug("Setting up Visonic Alarm integration.")
-    # Initialize the HUB as a global variable
-    global HUB
-    if not HUB:
-        from visonic import alarm as visonicalarm
-        # Load the configuration from the config flow
-        hub_config = config[DOMAIN]
-        HUB = VisonicAlarmHub(hub_config, visonicalarm)
-        if not HUB.connect():
-            return False
-        HUB.update()
-
-    # Load the supported platforms
-    for component in ('sensor', 'alarm_control_panel'):
-        discovery.load_platform(hass, component, DOMAIN, {}, config)
-
-    return True
