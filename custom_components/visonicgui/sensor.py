bcec80250b12f74a9299a9027c3ee362df500cca
@Aman <59781323+aman-sandhu@users.noreply.github.com>
2025-11-18 22:15:14 +0000

    Update sensor.py

--- custom_components/visonicgui/sensor.py
+++ custom_components/visonicgui/sensor.py
@@ -1,11 +1,8 @@
-# custom_components/visonicgui/sensor.py
-
 import logging
 from datetime import timedelta
-
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import STATE_CLOSED, STATE_OPEN, STATE_UNKNOWN
-from . import DOMAIN
+import asyncio
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,9 +27,9 @@ class VisonicAlarmSensor(Entity):
         """Return the state of the sensor."""
         return self._state
 
-    def update(self):
-        """Update sensor state."""
-        sensor_data = self._alarm.get_sensor_data(self._sensor_id)
+    async def async_update(self):
+        """Update sensor state asynchronously."""
+        sensor_data = await self._get_sensor_data(self._sensor_id)
 
         if sensor_data is None:
             self._state = STATE_UNKNOWN
@@ -40,3 +37,8 @@ class VisonicAlarmSensor(Entity):
             self._state = STATE_CLOSED
         elif sensor_data == "open":
             self._state = STATE_OPEN
+
+    async def _get_sensor_data(self, sensor_id):
+        """Fetch sensor data asynchronously."""
+        # If this function is doing blocking I/O, make it async.
+        return await self._alarm.get_sensor_data(sensor_id)
