de0aedf18c31189f6c8efda0db854c04d848ebe7
@Aman <59781323+aman-sandhu@users.noreply.github.com>
2025-11-18 22:31:32 +0000

    Update config_flow.py

--- custom_components/visonicgui/config_flow.py
+++ custom_components/visonicgui/config_flow.py
@@ -1,46 +1,68 @@
 import logging
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_PORT
-import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_APP_ID, CONF_USER_CODE, CONF_USER_EMAIL, CONF_USER_PASSWORD, CONF_PANEL_ID
+from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-class VisonicConfigFlow(config_entries.ConfigFlow):
-    """Handle a config flow for the Visonic integration."""
+class VisonicAlarmConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle the configuration flow for the Visonic Alarm integration."""
 
     VERSION = 1
 
     def __init__(self):
         """Initialize the config flow."""
         self._host = None
-        self._port = None
+        self._app_id = None
+        self._user_code = None
+        self._user_email = None
+        self._user_password = None
+        self._panel_id = None
+        self._partition = "ALL"
+        self._name = "Visonic Alarm"
 
     async def async_step_user(self, user_input=None):
-        """Handle the user input."""
-        if user_input is not None:
-            # Log the inputs
-            _LOGGER.debug("User input: %s", user_input)
-
+        """Handle the initial user step."""
+        if user_input:
             self._host = user_input[CONF_HOST]
-            self._port = user_input[CONF_PORT]
-            
-            # Create a new entry with the user input data
-            return self.async_create_entry(title="Visonic", data=user_input)
+            self._app_id = user_input[CONF_APP_ID]
+            self._user_code = user_input[CONF_USER_CODE]
+            self._user_email = user_input[CONF_USER_EMAIL]
+            self._user_password = user_input[CONF_USER_PASSWORD]
+            self._panel_id = user_input[CONF_PANEL_ID]
+            self._partition = user_input.get("partition", "ALL")
+            self._name = user_input.get("name", "Visonic Alarm")
 
-        # Display a form for the user to enter configuration data
+            # Create the configuration entry
+            return self.async_create_entry(
+                title=self._name,
+                data={
+                    CONF_HOST: self._host,
+                    CONF_APP_ID: self._app_id,
+                    CONF_USER_CODE: self._user_code,
+                    CONF_USER_EMAIL: self._user_email,
+                    CONF_USER_PASSWORD: self._user_password,
+                    CONF_PANEL_ID: self._panel_id,
+                    "partition": self._partition,
+                    "name": self._name,
+                },
+            )
+
+        # If no user input yet, show the form
         return self.async_show_form(
-            step_id="user", data_schema=self._get_user_input_schema()
+            step_id="user",
+            data_schema=self._get_user_input_schema(),
         )
 
     def _get_user_input_schema(self):
         """Return the schema for user input."""
-        return cv.Schema({
-            CONF_HOST: cv.string,
-            CONF_PORT: cv.port
+        return vol.Schema({
+            vol.Required(CONF_HOST): str,
+            vol.Required(CONF_APP_ID): str,
+            vol.Required(CONF_USER_CODE): str,
+            vol.Required(CONF_USER_EMAIL): str,
+            vol.Required(CONF_USER_PASSWORD): str,
+            vol.Required(CONF_PANEL_ID): str,
+            vol.Optional("partition", default="ALL"): str,
+            vol.Optional("name", default="Visonic Alarm"): str,
         })
-
-# Make sure this is the correct way to load the entry handler (not missing)
-async def async_setup_entry(hass, config_entry):
-    """Set up the integration from a config entry."""
-    _LOGGER.debug("Setting up Visonic config entry: %s", config_entry)
-    return True
